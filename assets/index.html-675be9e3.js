452b5062cde29a5c25e9d6ceacc7c8b588b1b455
annlev <71260515+annlev@users.noreply.github.com>
2024-03-15 12:48:20 +0000

    Deploying to gh-pages from  @ e8b862d25f2c08ed4e83b6581dd8df664ef11d2b 🚀

--- assets/index.html-675be9e3.js
+++
@@ -1,1 +0,0 @@
-const a=JSON.parse('{"key":"v-77964adc","path":"/admin/installation/","title":"Installation","lang":"en-US","frontmatter":{},"headers":[]}');export{a as data};
---
+++ assets/index.html-cfdca1f3.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-77964adc","path":"/admin/installation/","title":"Installation","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"Switch license","slug":"switch-license","link":"#switch-license","children":[]}]}');export{e as data};
