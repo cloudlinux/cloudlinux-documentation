80fef5509fdba38254fdfc8bf6564bf2d3fe8054
Bogdan Shyshka <bshyshka@gmail.com>
2026-07-29 11:33:25 +0300

    Remove 'Forum' link from bottomLinks.ts
    Removed 'Forum' link from bottom links configuration.

--- docs/.vuepress/config-client/bottomLinks.ts
+++ docs/.vuepress/config-client/bottomLinks.ts
@@ -4,10 +4,6 @@ export default [
         url: "https://cloudlinux.zendesk.com/hc/en-us/categories/360002375940"
     },
     {
-        text: "Forum",
-        url: "https://forum.cloudlinux.com/"
-    },
-    {
         text: "Blog",
         url: "https://blog.cloudlinux.com/"
     },
